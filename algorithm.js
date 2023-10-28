b4a720b8d364632c9c0fdbdcb567d4d9b6fb6466
Vyshnav R <vyshnavr856@gmail.com>
2023-10-28 17:11:07 +0530

    added js version of algo

--- algorithm.js
+++ algorithm.js
@@ -1,40 +1,37 @@
-// abhi nee ithil vende pole data fetch cheyyenam
-// nyan thalkalam data ndenn oro variables aayit assume cheyyan
+function assignTicket(ticketData) {
+	const allDevs = fetch("all devs in DB where ticketData.domain == dev.domain OR dev.domain == fullstack")
+	const allWorkLevels = []
 
-function allocateTicket(ticket) {
-	const allDevs = null // fetch all data of all devs, should be an array of data
-	let workLevels = []
 	for (const dev of allDevs) {
-		const allDevTickets = null // fetch all tickets of this particular dev
+		let workLevel = 0
 
-		let work = 0
-		work += ticket.difficulty * 25
-		work -= ticket.daysToComplete * 10
-		work -+ dev.skillLevel * 50
-
-
-		for (const devTicket of allDevTickets) {
-			if (devTicket.deadline != ticket.deadline) {
-				work += devTicket.difficulty * 25
-				work -= devTicket.daysToComplete * 10
-			}
-
-			else {
-				work += 100
-			}
+		// if dev is fullstack, add points to workLevel
+		if (dev.domain === "fullstack") {
+			workLevel += 20
 		}
 
-		workLevels.push([dev, work])
-	}
+		// add difficulty and deadline of current ticket to workLevel
+		workLevel += ticketData.difficulty * 20
+		workLevel -= ticketData.daysLeft * 10
 
-	let minDev = null
-	let min = 999
-	for (wl in workLevels) {
-		if (wl[1] < min) {
-			min = wl[1]
-			minDev = wl[0]
+		// factor in skill level of dev into workLevel
+		workLevel -= dev.skillLevel * 50
+
+
+		// add the difficulty of each ticket the current dev has to workLevel
+		const devTickets = fetch('all tickets the current dev has')
+
+		for (const devTicket of devTickets) {
+			workLevel += devTicket.difficulty * 20
+			workLevel -= devTicket.daysLeft * 10
 		}
-	}
 
-	//assign ticket to dev inside minDev
+		// add the current work level to array of all work levels
+		allWorkLevelspush(workLeve)
+	}
+	
+	// sort the allWorkLevels array in ascending order
+	const devWithLeastWork = allWorkLevels[0]
+
+	// assign ticket to devWithLeastWork
 }
--- manage/manage.js
+++ manage/manage.js
@@ -14,25 +14,25 @@ cancelTicketCreateButton.addEventListener("click", () => {
 	createTicketOverlay.style.display = "none"
 })
 
-createTicketForm.addEventListener("submit", (e) => {
-	e.preventDefault()
+// createTicketForm.addEventListener("submit", (e) => {
+// 	e.preventDefault()
 
-	const name = document.querySelector("#task-name").value
-	const taskType = document.querySelector("input[name='t-type']:checked").value
-	const priority = document.querySelector("input[name='priority']:checked").value
-	const difficulty = Number(document.querySelector("#difficulty").value)
-	const deadline = Number(document.querySelector("#deadline").value)
-	const dev = document.querySelector("#dev")
-	const selectedDev = dev.options[dev.selectedIndex].value
+// 	const name = document.querySelector("#task-name").value
+// 	const taskType = document.querySelector("input[name='t-type']:checked").value
+// 	const priority = document.querySelector("input[name='priority']:checked").value
+// 	const difficulty = Number(document.querySelector("#difficulty").value)
+// 	const deadline = Number(document.querySelector("#deadline").value)
+// 	const dev = document.querySelector("#dev")
+// 	const selectedDev = dev.options[dev.selectedIndex].value
 
-	const formData = {
-		name,
-		taskType,
-		priority,
-		difficulty,
-		deadline,
-		selectedDev,
-	}
+// 	const formData = {
+// 		name,
+// 		taskType,
+// 		priority,
+// 		difficulty,
+// 		deadline,
+// 		selectedDev,
+// 	}
 
-	console.log(formData);
-})
+// 	console.log(formData);
+// })
